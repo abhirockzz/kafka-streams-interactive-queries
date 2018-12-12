92ad397bc501d40df8e755e766802d0c2c404650
abhishek <>
2018-12-12 16:22:18 +0530

    minor updates

--- producer-test-data.sh
+++ producer-test-data.sh
@@ -5,6 +5,3 @@ docker run \
 confluentinc/cp-kafka \
 kafka-console-producer --request-required-acks 1 \
 --broker-list kafka:9092 --property "parse.key=true" --property "key.separator=:" --topic cpu-metrics-topic
-
-
-# docker run -i --net=confluent --rm confluentinc/cp-kafka kafka-console-consumer --bootstrap-server kafka:9092 --topic hashtag-count-topic --from-beginning --property print.key=true
